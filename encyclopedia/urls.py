21e1d79db512c55052d341d3f30b64dfaf5c7ba9
mtmahe <mtmahe@gmail.com>
2021-07-13 21:34:34 -0300

    search working

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.entry, name="entry"),
-    path("search", views.search, name = "search")
+    path("wiki/<str:name>", views.entry, name="entry"),
+    path("search", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,17 +1,30 @@
 import markdown2
 
 from django.shortcuts import render
+from django import forms
 
 from . import util
 
 
+
+class SearchForm(forms.Form):
+    item = forms.CharField(label="New Search")
+
+
+
+
+
 def index(request):
+    """ Show a list of links to entries and a search bar. """
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 
 def entry(request, name):
+    """ Render a page for the specific wiki entry. Give error if not found. """
+
     template = util.get_entry(name)
     if template:
         entry = markdown2.markdown(template)
@@ -24,16 +37,32 @@ def entry(request, name):
 
 
 def search(request):
-    form = Form(request.POST)
-    template = util.get_entry(form.cleaned_data["q"])
-    if template:
-        entry = markdown2.markdown(template)
-        url = "encyclopedia/entry.html"
-    else:
-        url = "encyclopedia/search.html"
-        entries = util.list_entries()
-        entry = [x for x in entries if x.rfind(name) != -1]
+    """When search bar used, link to exact match entry page. If no exact match,
+    provide list of entries that contain search parameter."""
 
-    return render(request, url, {
-        "entry": entry
-    })
+    # Get value of GET variable with name "q", if doesn't exist return ""
+    query = request.GET.get("q", "")
+
+    # Check if search field empty
+    if query == "":
+        return render(request, "encyclopedia/search.html", {
+            "query": query,
+            "matches": ""
+        })
+
+    # Check if exact match
+    entry = util.get_entry(query)
+    if entry != None:
+        entry = markdown2.markdown(entry)
+        return render(request, "encyclopedia/entry.html", {
+            "entry": entry
+        })
+
+    # Provide partial matches
+    else:
+        entries = util.list_entries()
+        matches = [x for x in entries if x.rfind(query) != -1]
+        return render(request, "encyclopedia/search.html", {
+            "matches": matches,
+            "query": query
+        })
